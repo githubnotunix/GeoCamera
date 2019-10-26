52dede7d544bc69c37821d61c4411894d6cbea96
githubnotunix <githubnotunix@github.com>
2019-10-26 16:40:43 -0500

    First Commit

---
+++ src/main/java/com/example/GoogleMaps/MapsActivity.java
@@ -0,0 +1,260 @@
+package com.example.GoogleMaps;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    Button camera;
+    String mCurrentPhotoPath;
+    ImageView imageView;
+    File photoFile = null;
+    //private FuseLocationProviderClient client;
+    static final int CAPTURE_IMAGE_REQUEST = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        camera = (Button) findViewById(R.id.button);
+
+        camera.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                if(Build.VERSION.SDK_INT >= 23){
+                    captureImage();
+                }
+                else{
+                   // captureImage2();
+                }
+            }
+        });
+        imageView = (ImageView) findViewById(R.id.image);
+    }
+    /* Capture Image function for 4.4.4 and lower. Not tested for Android Version 3 and 2 */
+    /*private void captureImage2() {
+
+        try {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            photoFile = createImageFile4();
+            if(photoFile!=null)
+            {
+                displayMessage(getBaseContext(),photoFile.getAbsolutePath());
+                Log.i("BBBB",photoFile.getAbsolutePath());
+                Uri photoURI  = Uri.fromFile(photoFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, CAPTURE_IMAGE_REQUEST);
+            }
+        }
+        catch (Exception e)
+        {
+            displayMessage(getBaseContext(),"Camera is not available."+e.toString());
+        }
+    }*/
+
+    private void captureImage()
+    {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+        else
+        {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                // Create the File where the photo should go
+                try {
+
+                    photoFile = createImageFile();
+                    displayMessage(getBaseContext(),photoFile.getAbsolutePath());
+                    Log.i("AAAAA",photoFile.getAbsolutePath());
+
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(this,
+                                "com.example.GoogleMaps.fileprovider",
+                                photoFile);
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST);
+                    }
+                } catch (Exception ex) {
+                    // Error occurred while creating the File
+                    displayMessage(getBaseContext(),ex.getMessage().toString());
+                }
+
+
+            }else
+            {
+                displayMessage(getBaseContext(),"Nullll");
+            }
+        }
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Bundle extras = data.getExtras();
+        //Bitmap imageBitmap = (Bitmap) extras.get("data");
+        //imageView.setImageBitmap(imageBitmap);
+
+        if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            Bitmap myBitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            imageView.setImageBitmap(myBitmap);
+        }
+        else
+        {
+            displayMessage(getBaseContext(),"Request cancelled or something went wrong.");
+        }
+    }
+
+   /* private File createImageFile4()
+    {
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                displayMessage(getBaseContext(),"Unable to create directory.");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+
+    }*/
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void displayMessage(Context context, String message)
+    {
+        Toast.makeText(context,message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                captureImage();
+            }
+        }
+
+    }
+    /*protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == 1){
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+    private void dispatchPictureTakerAction(){
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePic.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            photoFile = createPhotoFile();
+            if(photoFile != null){
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(MapsActivity.this, "com.example.GoogleMaps.fileprovider", photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePic, 1);
+            }
+
+        }
+    }
+
+    private File createPhotoFile(){
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try{
+            image = File.createTempFile(name, ".jpg", storageDir);
+        }
+        catch(Exception e){
+            Log.d("mylog", "Excep :" + e.toString());
+        }
+        return image;
+    }*/
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(36.0822, -94.1719);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
